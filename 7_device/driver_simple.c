c26a0ccce2b1479679700f4b6762b1560010bbd2
Maze ma <maze.ma1251228@gmail.com>
2016-04-21 14:45:42 +0800

    make with some warning

--- 7_device/driver_simple.c
+++ 7_device/driver_simple.c
@@ -10,9 +10,10 @@
 #include <linux/cdev.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
-#include <driver_simple.h>
 
 #include "driver_simple.h"
+MODULE_AUTHOR("Maze.ma");
+MODULE_LICENSE("Dual BSD/GPL");
 
 static int driversimple_major = DRIVERSIMPLE_MAJOR;
 module_param(driversimple_major, int, S_IRUGO);
@@ -56,24 +57,24 @@ static int driversimple_ioctl(struct inode *inodep, struct file *filp, unsigned	
 		if (err) 
 				return -EFAULT;
 
-		//struct driversimple_dev *dev = filp->private_data;/*get point*/
+		struct driversimple_dev *dev = filp->private_data;/*get point*/
 
 		switch (cmd)
 		{
 				case DRIVERSIMPLE_PRINT_PROCESS:
-						memset(dev->mem, 0, GLOBALMEM_SIZE);      
+						memset(dev->mem, 0, DRIVERSIMPLE_SIZE);      
 						printk(KERN_INFO "globalmem is set to zero/n");
 						break;
 				case DRIVERSIMPLE_SYS_CALL:
-						memset(dev->mem, 0, GLOBALMEM_SIZE);      
+						memset(dev->mem, 0, DRIVERSIMPLE_SIZE);      
 						printk(KERN_INFO "globalmem is set to zero/n");
 						break;
 				case DRIVERSIMPLE_SYS_IRT:
-						memset(dev->mem, 0, GLOBALMEM_SIZE);      
+						memset(dev->mem, 0, DRIVERSIMPLE_SIZE);      
 						printk(KERN_INFO "globalmem is set to zero/n");
 						break;
 				case DRIVERSIMPLE_SYS_ROAMING:
-						memset(dev->mem, 0, GLOBALMEM_SIZE);      
+						memset(dev->mem, 0, DRIVERSIMPLE_SIZE);      
 						printk(KERN_INFO "globalmem is set to zero/n");
 						break;
 
@@ -84,20 +85,20 @@ static int driversimple_ioctl(struct inode *inodep, struct file *filp, unsigned	
 }
 
 /*read function*/
-static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size,
+static ssize_t driversimple_read(struct file *filp, char __user *buf, size_t size,
 				loff_t *ppos)
 {
 		unsigned long p =  *ppos;
 		unsigned int count = size;
 		int ret = 0;
-		struct globalmem_dev *dev = filp->private_data; /*get point*/
+		struct driversimple_dev *dev = filp->private_data; /*get point*/
 
-		/*P>globalmem_size,too big*/
-		//if (p >= GLOBALMEM_SIZE)
-		if (p > GLOBALMEM_SIZE)
+		/*P>DRIVERSIMPLE,too big*/
+		//if (p >= DRIVERSIMPLE_SIZE)
+		if (p > DRIVERSIMPLE_SIZE)
 				return count ?  - ENXIO: 0;
-		if (count > GLOBALMEM_SIZE - p)
-				count = GLOBALMEM_SIZE - p;
+		if (count > DRIVERSIMPLE_SIZE - p)
+				count = DRIVERSIMPLE_SIZE - p;
 
 		/*kernel space->user space*/
 		if (copy_to_user(buf, (void*)(dev->mem + p), count))
@@ -116,18 +117,18 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size,
 }
 
 /*write function*/
-static ssize_t globalmem_write(struct file *filp, const char __user *buf,
+static ssize_t driversimple_write(struct file *filp, const char __user *buf,
 				size_t size, loff_t *ppos)
 {
 		unsigned long p =  *ppos;
 		unsigned int count = size;
 		int ret = 0;
-		struct globalmem_dev *dev = filp->private_data; 
+		struct driversimple_dev *dev = filp->private_data; 
 
-		if (p >= GLOBALMEM_SIZE)
+		if (p >= DRIVERSIMPLE_SIZE)
 				return count ?  - ENXIO: 0;
-		if (count > GLOBALMEM_SIZE - p)
-				count = GLOBALMEM_SIZE - p;
+		if (count > DRIVERSIMPLE_SIZE - p)
+				count = DRIVERSIMPLE_SIZE - p;
 
 		if (copy_from_user(dev->mem + p, buf, count))
 				ret =  - EFAULT;
@@ -143,7 +144,7 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
 }
 
 /* seek文件定位函数 */
-static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+static loff_t driversimple_llseek(struct file *filp, loff_t offset, int orig)
 {
 		loff_t ret = 0;
 		switch (orig)
@@ -154,7 +155,7 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 								ret =  - EINVAL;
 								break;
 						}
-						if ((unsigned int)offset > GLOBALMEM_SIZE)
+						if ((unsigned int)offset > DRIVERSIMPLE_SIZE)
 						{
 								ret =  - EINVAL;
 								break;
@@ -163,7 +164,7 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 						ret = filp->f_pos;
 						break;
 				case 1:   /*相对文件当前位置偏移*/
-						if ((filp->f_pos + offset) > GLOBALMEM_SIZE)
+						if ((filp->f_pos + offset) > DRIVERSIMPLE_SIZE)
 						{
 								ret =  - EINVAL;
 								break;
@@ -183,58 +184,58 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 		return ret;
 }
 
-static const struct file_operations globalmem_fops =
+static const struct file_operations driversimple_fops =
 {
 		.owner = THIS_MODULE,
-		.llseek = globalmem_llseek,
-		.read = globalmem_read,
-		.write = globalmem_write,
-		//.compat_ioctl = globalmem_ioctl,
-		.unlocked_ioctl = globalmem_ioctl,
-		.open = globalmem_open,
-		.release = globalmem_release,
+		.llseek = driversimple_llseek,
+		.read = driversimple_read,
+		.write = driversimple_write,
+		//.compat_ioctl = driversimple_ioctl,
+		.unlocked_ioctl = driversimple_ioctl,
+		.open = driversimple_open,
+		.release = driversimple_release,
 };
 
 /*init cdev*/
-static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
+static void driversimple_setup_cdev(struct driversimple_dev *dev, int index)
 {
-		int err, devno = MKDEV(globalmem_major, index);
+		int err, devno = MKDEV(driversimple_major, index);
 
-		cdev_init(&dev->cdev, &globalmem_fops);
+		cdev_init(&dev->cdev, &driversimple_fops);
 		dev->cdev.owner = THIS_MODULE;
-		dev->cdev.ops = &globalmem_fops;
+		dev->cdev.ops = &driversimple_fops;
 		err = cdev_add(&dev->cdev, devno, 1);
 		if (err)
 				printk(KERN_NOTICE "Error %d adding LED%d", err, index);
 }
 
 /*设备驱动模块加载函数*/
-static int __init  globalmem_init(void)
+static int __init  driversimple_init(void)
 {
 		int result;
-		dev_t devno = MKDEV(globalmem_major, 0);
+		dev_t devno = MKDEV(driversimple_major, 0);
 
 		/* 申请设备号*/
-		if (globalmem_major)
-				result = register_chrdev_region(devno, 1, "globalmem");
+		if (driversimple_major)
+				result = register_chrdev_region(devno, 1, "driversimple");
 		else  /* 动态申请设备号 */
 		{
-				result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
-				globalmem_major = MAJOR(devno);
+				result = alloc_chrdev_region(&devno, 0, 1, "driversimple");
+				driversimple_major = MAJOR(devno);
 		}  
 		if (result < 0)
 				return result;
 
 		/* 动态申请设备结构体的内存*/
-		globalmem_devp = kmalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
-		if (!globalmem_devp)    /*申请失败*/
+		driversimple_devp = kmalloc(sizeof(struct driversimple_dev), GFP_KERNEL);
+		if (!driversimple_devp)    /*申请失败*/
 		{
 				result =  - ENOMEM;
 				goto fail_malloc;
 		}
 		//  memset(globalmem_devp, 0, sizeof(struct globalmem_dev));
 
-		globalmem_setup_cdev(globalmem_devp, 0);
+		driversimple_setup_cdev(driversimple_devp, 0);
 		return 0;
 
 fail_malloc: 
@@ -243,18 +244,16 @@ fail_malloc:
 }
 
 /*模块卸载函数*/
-static void __exit globalmem_exit(void)
+static void __exit driversimple_exit(void)
 {
-		cdev_del(&globalmem_devp->cdev);   /*注销cdev*/
-		kfree(globalmem_devp);     /*释放设备结构体内存*/
-		unregister_chrdev_region(MKDEV(globalmem_major, 0), 1); /*释放设备号*/
+		cdev_del(&driversimple_devp->cdev);   /*注销cdev*/
+		kfree(driversimple_devp);     /*释放设备结构体内存*/
+		unregister_chrdev_region(MKDEV(driversimple_major, 0), 1); /*释放设备号*/
 }
 
-MODULE_AUTHOR("Song Baohua");
-MODULE_LICENSE("Dual BSD/GPL");
 
 //module_param(globalmem_major, int, S_IRUGO);
 
-module_init(globalmem_init);
-module_exit(globalmem_exit);
+module_init(driversimple_init);
+module_exit(driversimple_exit);
 
