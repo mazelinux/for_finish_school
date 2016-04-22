3ca57dedf8cbbd7e645ad69e1949c11e24c03ab9
Maze ma <maze.ma1251228@gmail.com>
2016-04-22 15:56:46 +0800

    find error reasion on show with seq->private

--- 7_device/driver_simple.c
+++ 7_device/driver_simple.c
@@ -217,50 +217,61 @@ static const struct file_operations driversimple_fops =
 //-----------------proc----------------//
 //-----------------proc----------------//
 //-----------------proc----------------//
+static char *str = NULL;
 
 static int driversimple_proc_show(struct seq_file *seq, void *v)
 {
-	unsigned int *ptr_var = seq->private;
-//	seq_printf(seq, "%u\n", *ptr_var);
-	return 0;
+		seq_printf(seq, "%s\n", str);
+		return 0;
 }
 
 static ssize_t driversimple_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *ppos)
 {
-	char *stop_at = NULL;
-	struct seq_file *seq = file->private_data;
-	unsigned int *ptr_var = seq->private;
-	*ptr_var = simple_strtoul(buffer, &stop_at, 10);
- 	return count;
+		//struct seq_file *seq = file->private_data;
+		//unsigned int *ptr_var = seq->private;
+		//*ptr_var = simple_strtoul(buffer, NULL, 10);
+		//return count;
+		//with error about null point
+		char *tmp = kzalloc((count+1), GFP_KERNEL);  
+		if(!tmp)  
+				return -ENOMEM;  
+		if(copy_from_user(tmp, buffer, count))  
+		{  
+				kfree(tmp);  
+				return EFAULT;  
+		}  
+		kfree(str);  
+		str = tmp;  
+		return count; 
 }
 
 static int driversimple_proc_open(struct inode *inode, struct file *file)
 {
- 	return single_open(file, driversimple_proc_show, PDE_DATA(inode));
+		return single_open(file, driversimple_proc_show, PDE_DATA(inode));
 }
 
 static const struct file_operations driversimple_proc_fops =
 {
-	.owner = THIS_MODULE,
-	.open = driversimple_proc_open,
-	.read =	seq_read,
-	.write = driversimple_proc_write,
-	.llseek = seq_lseek,
-	.release = single_release,
+		.owner = THIS_MODULE,
+		.open = driversimple_proc_open,
+		.read =	seq_read,
+		.write = driversimple_proc_write,
+		.llseek = seq_lseek,
+		.release = single_release,
 };
 
 static int driversimple_create_proc(void)
 {
-	struct proc_dir_entry *entry;
-	entry = proc_create_data("dirversimple_file", 0, NULL, &driversimple_proc_fops, NULL);//date=1,printk;date=2,syscall...
-	//entry = proc_create_entry("dirversimple_file", 0, NULL);//date=1,printk;date=2,syscall...
-	if(entry)
-		return 0;
+		struct proc_dir_entry *entry;
+		entry = proc_create_data("dirversimple_file", 0, NULL, &driversimple_proc_fops, NULL);//date=1,printk;date=2,syscall...
+		//entry = proc_create_entry("dirversimple_file", 0, NULL);//date=1,printk;date=2,syscall...
+		if(entry)
+				return 0;
 }
 
 static void driversimple_remove_proc(void)
 {
-	remove_proc_entry("dirversimple_file", NULL);
+		remove_proc_entry("dirversimple_file", NULL);
 }
 
 //-----------------proc----------------//
@@ -314,7 +325,7 @@ static int __init  driversimple_init(void)
 fail_malloc: 
 		unregister_chrdev_region(devno, 1);
 		return result;
-	
+
 }
 
 /*模块卸载函数*/
